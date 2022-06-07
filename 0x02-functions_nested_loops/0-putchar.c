d1f06eba8947175c22bb8aff2ffb76176fa78fde
Adonisenny <funkmater@gmail.com>
2022-06-07 06:41:05 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,15 +1,16 @@
 #include "main.h"
 /**
- * main - Entry point
- * Return:0
+ * main - Print "Holberton"
+ *
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-char c[10] = "Holberton";
 int i = 0;
+char name_of_school[] = "_putchar";
 while (i < 9)
 {
-_putchar(c[i]);
+_putchar(name_of_school[i]);
 i++;
 }
 _putchar('\n');
